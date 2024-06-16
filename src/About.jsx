5df13420d742dcb741b31a66ad2e6534e4a7f3a2
Nischal <nischalshrestha800@gmail.com>
2024-06-16 19:46:06 +0545

    Managed Route Problems

---
+++ src/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button'
+
+const About = () => {
+    return (
+        <div>
+            <Button />
+            <Button />
+            <Button />
+        </div>
+    )
+}
+
+export default About
---
+++ src/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+
+const Contact = () => {
+    return (
+        <div>
+            <Button />
+            <Button />
+            <Button />
+
+        </div>
+    )
+}
+
+export default Contact
